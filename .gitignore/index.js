b553b0c63d73ac4026383303f3862d46d0f3c8c2
Willz-dev <73960916+Willz-dev@users.noreply.github.com>
2021-01-27 13:38:45 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,7 +6,7 @@ const Discord = require('discord.js'),
     config = require('./config.json'),
     fs = require('fs')
  
-client.login(config.token)
+client.login(process.env.TOKEN)
 client.commands = new Discord.Collection()
  
 fs.readdir('./commands', (err, files) => {
